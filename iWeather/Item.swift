77215c6e5e2ceb43d11acd58044f62af38be6fc1
siraajul <shirajulislamparvez@gmail.com>
2024-02-18 14:11:52 +0600

    Initial Commit

---
+++ iWeather/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  iWeather
+//
+//  Created by Sirajul Islam on 18/2/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
